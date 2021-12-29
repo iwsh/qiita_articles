ba0bb1cd2dd01ea09c6b7744f5205633160f09c2
iwsh <kabwkkr.seanae@gmail.com>
2021-12-29 13:05:11 +0900

    記事の自動リリース作成（途中）

--- .github/scripts/develop_action.py
+++ .github/scripts/develop_action.py
@@ -1,14 +1,54 @@
 #!/usr/bin/env python
-import request
+import requests
+from modules import qiita
+import os
+import re
+import logging
 
-# Qiitaから投稿された記事の状態を取得
+workspace = os.environ["GITHUB_WORKSPACE"]
 
-# gitの更新履歴を確認
+def main():
+    # Qiitaから投稿された記事一覧を取得
+    l_articles_qiita = qiita.get_pages()
 
-# 記事の更新以降のgit更新を抽出
+    # git上の記事一覧を取得
+    l_article_private = os.listdir(os.path.join(workspace, "articles", "private"))
+    l_article_public = os.listdir(os.path.join(workspace, "articles", "public"))
 
-# 更新操作を実行
+    # privateの記事を作成・更新
+    for fname_article in l_article_private:
+        id_article = get_id_from_article(fname_article)
 
-# 完了
+        l_id = [article_qiita["id"] for article_qiita in l_articles_qiita]
+        if id_article in l_id:
+            # update処理
+            pass
+        else:
+            pass
+            # post処理
 
-print("ends.")
+            # responseのidをコメントで記事の1行目に記載             
+
+
+    # publicの記事は新規作成の場合のみ限定公開で作成
+    for fname_article in l_article_private:
+        id_article = get_id_from_article(fname_article)
+
+        l_id = [article_qiita["id"] for article_qiita in l_articles_qiita]
+        if id_article not in l_id:
+            pass
+            # post処理
+
+    # 完了
+    print("ends.")
+
+
+def get_id_from_article(fname_article):
+    # "<!-- Qiita記事を保管するリポジトリ -->"
+    with open(fname_article, "r") as f:
+        line = f.readline()
+    id_article = re.findall("<!-- ID:(.*) -->", line)
+    return id_article
+
+if __name__ == "__main__":
+    main()
